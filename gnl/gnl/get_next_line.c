1974409a42adb49f5bbbd789909858bbff046ebb
interfan-sys <agron10k@gmail.com>
2025-12-03 14:12:02 +0100

    Add files via upload

--- gnl/gnl/get_next_line.c
+++ gnl/gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agkicina <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 10:28:56 by agkicina          #+#    #+#             */
-/*   Updated: 2025/12/03 13:06:59 by agkicina         ###   ########.fr       */
+/*   Updated: 2025/12/03 14:10:53 by agkicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,33 @@
 #include <stdlib.h>
 #include <fcnlt.h>
 
+char	*ft_strchr(const char *s, int	c)
+{
+	while (s != '\0')
+		{
+			if (*s == (char )c)
+			{
+				return ((char *)s);
+			}
+			s++;
+		}
+		if ((char )c == '\0')
+			{
+				return ((char *)s);
+			}
+			return (NULL);
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*stash;
 	char	buffer[BUFFER_SIZE + 1];
 	ssize_t	bytes;
 	char	*newline;
-	int	i;
-	
-	i = 0;
+
 	bytes = read(fd, buffer, BUFFER_SIZE);
 	buffer[bytes] = '\0';
-	while ((!ft_strchr(stash, '\n') && buffer[i] != '\0') && bytes > 0)
-		i++;
+	while ((!ft_strchr(stash, '\n') && bytes > 0)
 		if (bytes <= 0)
 			return (NULL);
 
